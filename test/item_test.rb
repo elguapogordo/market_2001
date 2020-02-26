b061069c7bb05c41e22eedae3b24f7f169a50be2
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-26 10:39:37 -0700

    add item tests

---
+++ test/item_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+
+class ItemTest < Minitest::Test
+
+  def test_it_exists
+    item = Item.new({name: 'Peach', price: "$0.75"})
+
+    assert_instance_of Item, item
+  end
+
+  def test_it_has_attributes
+    item = Item.new({name: 'Peach', price: "$0.75"})
+
+    assert_equal "Peach", item.name
+    assert_equal "$0.75", item.price
+  end
+
+end
