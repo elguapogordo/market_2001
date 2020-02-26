a229c595669530de1be08cdae00ed103e11bbef9
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-26 10:26:02 -0700

    add setup, check_stock 0 test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -5,37 +5,35 @@ require './lib/item'
 
 class VendorTest < Minitest::Test
 
-  def test_it_exists
-    vendor = Vendor.new("Rocky Mountain Fresh")
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
 
-    assert_instance_of Vendor, vendor
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
   end
 
   def test_it_has_attributes
-    vendor = Vendor.new("Rocky Mountain Fresh")
-    item1 = Item.new({name: 'Peach', price: "$0.75"})
-    item2 = Item.new({name: 'Tomato', price: '$0.50'})
-
-    assert_equal "Tomato", item2.name
-    assert_equal "$0.50", item2.price
+    assert_equal "Tomato", @item2.name
+    assert_equal "$0.50", @item2.price
   end
 
   def test_it_has_a_name
-    vendor = Vendor.new("Rocky Mountain Fresh")
-
-    assert_equal "Rocky Mountain Fresh", vendor.name
+    assert_equal "Rocky Mountain Fresh", @vendor.name
   end
 
   def test_it_has_inventory
-    vendor = Vendor.new("Rocky Mountain Fresh")
-
-    assert_equal ({}), vendor.inventory
+    assert_equal ({}), @vendor.inventory
   end
 
   def test_inventory_default_value_is_0
-    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_equal 0, @vendor.inventory["Bananas"]
+  end
 
-    assert_equal 0, vendor.inventory["Bananas"]
+  def test_it_can_check_empty_stock_is_0
+    assert_equal 0, @vendor.check_stock(@item1)
   end
 
 end
