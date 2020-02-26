e9b1f916e094537677a4028d2fcae51ad28729de
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-26 11:51:48 -0700

    add total_inventory test and method

--- lib/market.rb
+++ lib/market.rb
@@ -5,6 +5,9 @@ class Market
   def initialize(name)
     @name = name
     @vendors = []
+    @inventory_hash = Hash.new do |hash, key|
+      hash[key] = Hash.new { |hash, key| hash[key] = 0 }
+    end
   end
 
   def add_vendor(vendor)
@@ -18,5 +21,19 @@ class Market
   def vendors_that_sell(item)
     vendors.find_all { |vendor| vendor.inventory.include?(item) }
   end
+
+  def sorted_items_list
+  end
+
+  def total_inventory
+    vendors.each do |vendor|
+      vendor.inventory.each do |item, qty|
+        @inventory_hash[item][:quantity] += qty
+        @inventory_hash[item][:vendors] = vendors_that_sell(item)
+      end
+    end
+    @inventory_hash
+  end
+  # require 'pry'; binding.pry
 
 end
